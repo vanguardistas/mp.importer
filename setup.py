1891d97af3cf6a1bf08930d6e8b73fcaf67d8219
Brian Sutherland <brian@vanguardistas.net>
2014-06-04 17:05:51 +0200

    project structure

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+_here = os.path.dirname(__file__)
+
+f = open(os.path.join(_here, 'README.md'), 'r')
+README = f.read()
+f.close()
+
+install_requires = ['lxml']
+if sys.version_info[0] == 2:
+    # python2 does not have mock in the standard lib
+    install_requires.append('mock')
+
+setup(name="mp.import",
+      version="0.1",
+      description="Utilities to ease imports of content to MetroPublisher.",
+      packages=find_packages(),
+      long_description=README,
+      license='BSD',
+      author="Vanguardistas LLC",
+      author_email='brian@vanguardistas.net',
+      install_requires=install_requires,
+      classifiers=[
+          "Intended Audience :: Developers",
+          "Operating System :: OS Independent",
+          "License :: OSI Approved :: BSD License",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          ],
+      test_suite="mp.import.tests",
+      )
