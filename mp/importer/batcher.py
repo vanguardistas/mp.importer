3d29767b83627753dc429042d5605ecbbc76ead1
kiara <saetachiara@gmail.com>
2015-03-24 13:52:56 +0100

    new version using iterator

--- mp/importer/batcher.py
+++ mp/importer/batcher.py
@@ -1,51 +1,27 @@
 # this function is intentionally get empty for now
-# this program takes an iterable and splits in a number of batches using the batch_size argument (from context)
-# result is a list that contains the batches (for now)
+# this program takes an iterable and splits in a number of batches using context arguments
+# result is a list that contains the batches (for now) - TODO pass iterator
+
+from itertools import *
+from itertools import count
 
 def run_in_batches(context, iterable, process_one): 		
-    num = int(len(iterable))                                      
     batch_size = context.batch_size
-    start_batch = context.start_batch					
-    batch_stop, extra = divmod(num, batch_size)
-    if start_batch == 0:
-        list = []
-        if extra == 0:								
-            for n in range(0,batch_stop):												
-                batch_item = iterable[n*batch_size:batch_size+n*batch_size] 	
-                result = process_one(batch_item)
-                list.append(result)
-        else:
-            for n in range(0,batch_stop):					
-                batch_item = iterable[n*batch_size:batch_size+n*batch_size]	
-                result = process_one(batch_item)
-                list.append(result)
-            last_item = iterable[batch_stop*batch_size:]
-            result = process_one(last_item)
-            list.append(result)
-        return list  
-    else:								# here starts from some batch specified by context (loops can be merged because it's a specific case when start_batch = 0)
-        list = []
-        if extra == 0:								
-            for n in range(start_batch,batch_stop):					
-                batch_item = iterable[n*batch_size:batch_size+n*batch_size]	
-                result = process_one(batch_item)
-                list.append(result)
-        else:
-            for n in range(start_batch,batch_stop):					
-                batch_item = iterable[n*batch_size:batch_size+n*batch_size]	
-                result = process_one(batch_item)
-                list.append(result)
-            last_item = iterable[batch_stop*batch_size:]
-            result = process_one(last_item)
-            list.append(result)
-        return list
+    start_batch = context.start_batch
+    max_batches = context.max_batches
+    start = start_batch*batch_size 
+    tot_result = []
+    for x in range (0,max_batches-1):											# for each batch creates a different list					
+        my_iterator = islice(iterable, start+x*batch_size, start+x*batch_size+batch_size, 1)				
+        batch = list(my_iterator)
+        result = process_one(batch)
+        if len(result) != 0:
+            tot_result.append(result)
+    return tot_result													# TODO  pass directly the iterator
 
-# TODO
-# https://docs.python.org/2/library/itertools.html see itertools documentation for random batcher
-#def random_combination(iterable, r):
- #   "Random selection from itertools.combinations(iterable, r)"
- #   pool = tuple(iterable)
- #   n = len(pool)
- #   indices = sorted(random.sample(xrange(n), r))
- #   return tuple(pool[i] for i in indices)
+# check what happens with max_batches (if they will be set by default on 100 for example)  -  avoid creating empty batches
+# if I return an iterator, the assertion would change and so the process_one
+
+
+
 
--- mp/importer/tests/test_batcher.py
+++ mp/importer/tests/test_batcher.py
@@ -3,55 +3,47 @@ from unittest import TestCase
 import collections
 from collections import namedtuple
 from collections import Iterable
+from itertools import *
 
-def make_test_context_1():									# creates a "provisional" context 
+def make_test_context(b_size, b_start, b_max):									# creates a "provisional" context 
     context = namedtuple('Context', """
         start_batch
+        max_batches
         batch_size """)
-    context.batch_size = 2									# batcher will read it from context in the script
-    context.start_batch = 0
-    return context
-
-def make_test_context_2():									 
-    context = namedtuple('Context', """
-        start_batch
-        batch_size """)
-    context.batch_size = 2									
-    context.start_batch = 2
+    context.batch_size = b_size
+    context.start_batch = b_start
+    context.max_batches = b_max
     return context
 
 # test function (it will be the import function) - generalize for different types of iterable - now writes results in a list with nested lists (to be changed if needed)
-def process_one(item):
+def process_one(item):				
     result = []
-    if isinstance(item, str):
-        result.append(item.upper())
+    if len(item) != 0:
+        if isinstance(item, str):
+            result.append(item.upper())
+        else:
+            for x in item:
+                result.append(x.upper()) 
+        return result
     else:
-        for x in item:
-            result.append(x.upper()) 
-    return result
-
-def process_one_simple(item):
-    result = []
-    result.append(item.upper())
-    return result
-# remember that batch_item is a piece of an iterable, done like this batch_item = iterable[n*batch_size:batch_size+n*batch_size] - can be done only for objects that have __getitem__
+        return result
 
 class TestBatcher(TestCase):
     # 1- test that the function makes batches of a given size
     def test_Make_Batches(self):
-        input_string = 'abcdefg'									
-        context = make_test_context_1()
+        input_list = ['a','b','c','d'] 									
+        context = make_test_context(2,0,3)
         from .. import batcher									
-        result = batcher.run_in_batches(context, input_string, process_one_simple)				
-        self.assertEqual(result,[['AB'], ['CD'], ['EF'], ['G']])
+        result = batcher.run_in_batches(context, input_list, process_one)				
+        self.assertEqual(result,[['A','B'], ['C','D']])
     #
     # 2- test that the function starts being executed from a specified batch number (batches of given size)
     def test_Start_from_Batch(self):
-        input_string = 'abcdefg'									
-        context = make_test_context_2()
+        input_list = ['a','b','c','d','e','f']									
+        context = make_test_context(2,2,2)
         from .. import batcher									
-        result = batcher.run_in_batches(context, input_string, process_one_simple)				
-        self.assertEqual(result,[['EF'], ['G']])
+        result = batcher.run_in_batches(context, input_list, process_one)				
+        self.assertEqual(result,[['E', 'F']])
     #
     # 3- test if inputs of the function are iterables or not
     def test_Batcher_iterables(self):
@@ -68,7 +60,7 @@ class TestBatcher(TestCase):
         input_to_test = [test_set, test_list, test_string, test_tuple, test_file, test_No_obj, test_number] 
         for intest in input_to_test:
             try:
-                intest_iterator = iter(intest) 							# valid both for __iter__ and __getitem__ method (str and files respectively)
+                intest_iterator = iter(intest)		# iter(intest) valid both for __iter__ and __getitem__ method (str and files respectively) 
                 ok = ok + 1
             except:
                 not_ok = not_ok + 1
@@ -80,7 +72,7 @@ class TestBatcher(TestCase):
         test_list = list()
         test_list.append('a') 									# for now i use letters because i still do "upper"
         test_list.append('a')
-        test_list.append('x')
+        test_list.append('x')		# test_list = ('a','a','x'), test_set is same
         test_set = set(test_list)
         test_frozenset = frozenset()
         filename = '/home/kiara/GIT/mp.importer/mp/importer/tests/test.csv'
@@ -89,30 +81,25 @@ class TestBatcher(TestCase):
         for row in openedfile:
             test_row = row[:-1]									# in case of file, i read the lines of the file and create a list, that will be then splitted
             test_row_list.append(test_row)
-        input_to_test = ['', 'abcd', '111', test_set, test_frozenset, test_list, set(), list(), test_row_list]   # try list(), set(), tuples
+        input_to_test = [test_list, test_set, test_frozenset, set(), list(), test_row_list]   # try list(), set(), tuples
         from .. import batcher	
-        context = make_test_context_1()
+        context = make_test_context(2,0,3)
         result_list = list()								
         for i in input_to_test:
-            if hasattr(i, '__getitem__') is True: 						# __getitem__ is used by the batcher function!
+            if hasattr(i, '__getitem__') is True: 						# __getitem__ is used by the batcher function! replaced by next!
                 result = batcher.run_in_batches(context, i, process_one)			
                 result_list.append(result) 
-            else:										
+            else:	
+                #print i									
                 pass
-                #self.fail()									# non-iterable should fall in here - set and frozenset fall here because don't have __getitem__
-        self.assertEqual(result_list,[[], [['AB'], ['CD']], [['11'], ['1']], [['A','A'], ['X']], [], [['CHIARA', 'ANNA'], ['XXX', 'X']]])	
+                #self.fail()									# non-iterable should fall in here -  test_set, test_frozenset, set()
+        self.assertEqual(result_list,[[['A','A'], ['X']], [], [['CHIARA', 'ANNA'], ['XXX', 'X']]])	 # empty list gives empty batches - change in code to skip that
     #
-
 
 # if the csv is opened in bynary mode, py34 adds a 'b' in the string invalidating the assertion - for now reading in text mode
 # if not you could also use decode but does not seem to work - row.decode('utf8')
-# 
-# *** test 3-4:
-#isinstance(i, collections.Iterable): does not work for sets, but I'm using getitem!!! 
-# set and frozenset are not iterable, but TODO - try different ways because in test-3 they are considered iterable!!!!
-#try: intest_iterator = iter(i) also works
-# nb: hasattr(i, '__getitem__') is False: not valid for strings! same for __iter__, not valid for file!
-# self.assertIsInstance(i, collections.Iterable) could also be a test itself
+ 
+# input_to_test = ['', 'abcd', '111', test_set, test_frozenset, test_list, set(), list(), test_row_list]   # try list(), set(), tuples
 
  
 
