fd90790c1e59898fff5490c10b0d8543f0b629cd
kiara <saetachiara@gmail.com>
2015-03-25 10:43:50 +0100

    new version using yeld - work in progress

--- mp/importer/batcher.py
+++ mp/importer/batcher.py
@@ -1,27 +1,32 @@
 # this function is intentionally get empty for now
 # this program takes an iterable and splits in a number of batches using context arguments
-# result is a list that contains the batches (for now) - TODO pass iterator
+# result of the run_in_batches is that each batch element is yielded and passed to the process_one function 
 
 from itertools import *
 from itertools import count
 
-def run_in_batches(context, iterable, process_one): 		
-    batch_size = context.batch_size
-    start_batch = context.start_batch
-    max_batches = context.max_batches
-    start = start_batch*batch_size 
+def get_batches(context, iterable, nb):
+    count = 0
+    bsize = context.batch_size
+    ssize = context.start_batch
+    start = ssize*bsize
+    for i in iterable:
+        count += 1
+        if start+bsize*nb <= count-1 <= start++bsize*nb+bsize-1:     # for now takes only the first batch 
+            yield i
+
+def run_in_batches(context, iterable, process_one):
+    bmax = context.max_batches
     tot_result = []
-    for x in range (0,max_batches-1):											# for each batch creates a different list					
-        my_iterator = islice(iterable, start+x*batch_size, start+x*batch_size+batch_size, 1)				
-        batch = list(my_iterator)
-        result = process_one(batch)
-        if len(result) != 0:
-            tot_result.append(result)
-    return tot_result													# TODO  pass directly the iterator
-
-# check what happens with max_batches (if they will be set by default on 100 for example)  -  avoid creating empty batches
-# if I return an iterator, the assertion would change and so the process_one
+    for nbatch in range(0,bmax):				      # for each batch launches the pipeline (get-batches and process)
+        got_batch = get_batches(context, iterable, nbatch)
+        for k in got_batch:
+       	    result = process_one(k)
+            tot_result.append(result)						
+    return tot_result
 
 
-
+# TODO
+# yield got_batch.next() for next function in pipeline (statistics)
+# add try-except to call the end_batch function when each batch is over
 
--- mp/importer/tests/test_batcher.py
+++ mp/importer/tests/test_batcher.py
@@ -35,72 +35,15 @@ class TestBatcher(TestCase):
         context = make_test_context(2,0,3)
         from .. import batcher									
         result = batcher.run_in_batches(context, input_list, process_one)				
-        self.assertEqual(result,[['A','B'], ['C','D']])
+        self.assertEqual(result,[['A'],['B'],['C'],['D']])
     #
-    # 2- test that the function starts being executed from a specified batch number (batches of given size)
+    # 2- test that the function starts being executed from a specified batch number (batches of given size) and that the max_batches works
     def test_Start_from_Batch(self):
-        input_list = ['a','b','c','d','e','f']									
-        context = make_test_context(2,2,2)
-        from .. import batcher									
-        result = batcher.run_in_batches(context, input_list, process_one)				
-        self.assertEqual(result,[['E', 'F']])
-    #
-    # 3- test if inputs of the function are iterables or not
-    def test_Batcher_iterables(self):
-        ok = 0
-        not_ok = 0
-        test_tuple = ('a','b',1)
-        test_list = ['a','b',1]
-        test_set = set(test_list)    # set are iterable!
-        test_No_obj = None
-        test_number = 1233454
-        test_string = 'chiara'
-        filename = '/home/kiara/GIT/mp.importer/mp/importer/tests/test.csv'
-        test_file = open(filename, 'r') 
-        input_to_test = [test_set, test_list, test_string, test_tuple, test_file, test_No_obj, test_number] 
-        for intest in input_to_test:
-            try:
-                intest_iterator = iter(intest)		# iter(intest) valid both for __iter__ and __getitem__ method (str and files respectively) 
-                ok = ok + 1
-            except:
-                not_ok = not_ok + 1
-        self.assertEqual(ok, 5)
-        self.assertEqual(not_ok, 2)
-
-    # 4- check different iterable inputs (including empty) and non-iterable (e.g. set) to make it fail 
-    def test_Batcher_input(self):
-        test_list = list()
-        test_list.append('a') 									# for now i use letters because i still do "upper"
-        test_list.append('a')
-        test_list.append('x')		# test_list = ('a','a','x'), test_set is same
-        test_set = set(test_list)
-        test_frozenset = frozenset()
-        filename = '/home/kiara/GIT/mp.importer/mp/importer/tests/test.csv'
-        openedfile = open(filename, 'r')
-        test_row_list = list()
-        for row in openedfile:
-            test_row = row[:-1]									# in case of file, i read the lines of the file and create a list, that will be then splitted
-            test_row_list.append(test_row)
-        input_to_test = [test_list, test_set, test_frozenset, set(), list(), test_row_list]   # try list(), set(), tuples
-        from .. import batcher	
-        context = make_test_context(2,0,3)
-        result_list = list()								
-        for i in input_to_test:
-            if hasattr(i, '__getitem__') is True: 						# __getitem__ is used by the batcher function! replaced by next!
-                result = batcher.run_in_batches(context, i, process_one)			
-                result_list.append(result) 
-            else:	
-                #print i									
-                pass
-                #self.fail()									# non-iterable should fall in here -  test_set, test_frozenset, set()
-        self.assertEqual(result_list,[[['A','A'], ['X']], [], [['CHIARA', 'ANNA'], ['XXX', 'X']]])	 # empty list gives empty batches - change in code to skip that
-    #
-
-# if the csv is opened in bynary mode, py34 adds a 'b' in the string invalidating the assertion - for now reading in text mode
-# if not you could also use decode but does not seem to work - row.decode('utf8')
- 
-# input_to_test = ['', 'abcd', '111', test_set, test_frozenset, test_list, set(), list(), test_row_list]   # try list(), set(), tuples
-
+       input_list = ['a','b','c','d','e','f','g','h']									
+       context = make_test_context(2,2,1)
+       from .. import batcher									
+       result = batcher.run_in_batches(context, input_list, process_one)				
+       self.assertEqual(result,[['E'],['F']])
  
 
 
