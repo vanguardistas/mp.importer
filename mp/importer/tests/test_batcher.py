b58b51f1123589e61c21c463fc4f9f54d1ae00d6
kiara <saetachiara@gmail.com>
2015-03-25 17:18:35 +0100

    test with csv

--- mp/importer/tests/test_batcher.py
+++ mp/importer/tests/test_batcher.py
@@ -45,7 +45,33 @@ class TestBatcher(TestCase):
        result = batcher.run_in_batches(context, input_list, process_one)				
        self.assertEqual(result,['E','F'])
  
-
+    # 3- check different iterable inputs (including empty) and non-iterable (e.g. set) to make it fail 
+    def test_Batcher_input(self):
+        test_list = list()
+        test_list.append('a') 									# for now i use letters because i still do "upper"
+        test_list.append('a')
+        test_list.append('x')		# test_list = ('a','a','x'), test_set is same
+        test_set = set(test_list)
+        test_frozenset = frozenset()
+        filename = '/home/kiara/GIT/mp.importer/mp/importer/tests/test.csv'
+        openedfile = open(filename, 'r')
+        test_row_list = list()
+        for row in openedfile:
+            test_row = row[:-1]									# in case of file, i read the lines of the file and create a list, that will be then splitted
+            test_row_list.append(test_row)
+        input_to_test = [test_list, test_set, test_frozenset, set(), list(), test_row_list]   # try list(), set(), tuples
+        from .. import batcher	
+        context = make_test_context(2,0,3)
+        result_list = list()								
+        for i in input_to_test:
+            if hasattr(i, '__getitem__') is True: 						# __getitem__ is used by the batcher function! replaced by next!
+                result = batcher.run_in_batches(context, i, process_one)			
+                result_list.append(result) 
+            else:	
+                #print i									
+                pass
+                #self.fail()									# non-iterable should fall in here -  test_set, test_frozenset, set()
+        self.assertEqual(result_list,[['A','A', 'X'], [], ['CHIARA', 'ANNA', 'XXX', 'X']])	 # empty list gives this... [[], []] change in code to skip empty things!
 
 
 
