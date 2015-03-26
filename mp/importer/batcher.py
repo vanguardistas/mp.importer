7c52df29e75c73f987d97f4915af5ec978f9109a
kiara <saetachiara@gmail.com>
2015-03-26 15:20:45 +0100

    attempt of doing it - works for first batch

--- mp/importer/batcher.py
+++ mp/importer/batcher.py
@@ -12,48 +12,25 @@ def get_batches(context, iterable, nb):
     start = ssize*bsize
     for i in iterable:
         count += 1
-        if start+bsize*nb <= count-1 <= start++bsize*nb+bsize-1:      	# for now takes only the first batch 
+        if start+bsize*nb <= count-1 <= start+bsize*nb+bsize-1:      	# for now takes only the first batch 
+            print ("batch", i)
             yield i
 
-def run_in_batches(context, iterable, process_one):
+
+def run_in_batches(context, iterable, end_batch):
     bmax = context.max_batches
-    tot_tot_result = []
-    for nbatch in range(0,bmax):
-        tot_result = []				     			# for each batch launches the pipeline (get-batches and process)
-        got_batch = get_batches(context, iterable, nbatch)
+    nbatch = 0
+    while nbatch <= bmax:
+        print ('nbatch', nbatch)
+        got_batch = get_batches(context, iterable, nbatch)		# gets batch elements (yields) for each value of nbatch
+        c = 0 
+        nbatch = nbatch+1
         for k in got_batch:
-       	    result = process_one(k)					# tot_result.append(result) - instead of nesting lists, merge
-            if result == 'skip':
-                continue						# skip empty elements
-            tot_result += result
-            #yield k							# yield for statistics function (in pipeline) but then i cannot return tot_result? TODO
-        if len(tot_result) > 0:
-            statistics(nbatch, tot_result)				# stastistics are done within each batch but i'd like to skip empty batches!
-            tot_tot_result += tot_result				# add the batch to the final result										
-            end_batch(context, nbatch)					# check if is ok to skip empty batches like that
-    return tot_tot_result
+            c = c+1						# in the yielded batch, appends X in the right place
+            yield k
+            if c == context.batch_size:
+                end_batch()						        # appends 'X' when batch ends
 
-# context.sess.add(result) is done out in the real process_one function (e.g. do_one)
 
-# call the end_batch function:
-def end_batch(context, nb):
-    print ("end batch number", nb+1)
-    pass
-    #context.sess.flush()
-
-# statistics performed for each batch - yield got_batch and use .next() for elements and call statistics(got_batch) TODO 
-def statistics(nb, res):					
-   ok = 0
-   not_ok = 0
-   tot = 0
-   for k in res:
-       if k:
-           tot = tot+1
-       if len(k) > 0:						# add option if result == 'skip' then continue
-           ok = ok+1
-       else:
-           not_ok = not_ok+1 
-   print ('FOR BATCH {} IMPORTED {} of {} elements ({:.1%} success rate)'.format(nb+1, ok, tot, ok/(float(tot) + 0.00001)))
-   #context.prob() # "problems for this batch are", not_ok
 
 
--- mp/importer/tests/test_batcher.py
+++ mp/importer/tests/test_batcher.py
@@ -7,95 +7,30 @@ from itertools import *
 
 def make_test_context(b_size, b_start, b_max):									# creates a "provisional" context 
     context = namedtuple('Context', """
+        batch_size
         start_batch
-        max_batches
-        batch_size """)
+        max_batches """)
     context.batch_size = b_size
     context.start_batch = b_start
     context.max_batches = b_max
     return context
 
-# test function (it will be the import function) - generalize for different types of iterable - now writes results in a list with nested lists (to be changed if needed)
-def process_one(item):				
-    result = []
-    if len(item) == 0 or item is None:
-        return 'skip'
-    else:
-        if isinstance(item, str):
-            result.append(item.upper())
-        else:
-            for x in item:
-                result.append(x.upper()) 
-        return result
-
 class TestBatcher(TestCase):
-    # 1- test that the function makes batches of a given size
-    def test_Make_Batches(self):
-        print ("test Make Batches")
-        input_list = ['a','b','c','d'] 									
-        context = make_test_context(2,0,2)			# there is no problem if end_batch is 100 because the loop ends when the iterator ends
-        from .. import batcher									
-        result = batcher.run_in_batches(context, input_list, process_one)				
-        self.assertEqual(result,['A','B','C','D'])
-    #
-    # 2- test that the function starts being executed from a specified batch number (batches of given size) and that the max_batches works
-    def test_Start_from_Batch(self):
-        print ("test Start from batch")
-        input_list = ['a','b','c','d','e','f','g','h']									
-        context = make_test_context(2,2,1)
-        from .. import batcher									
-        result = batcher.run_in_batches(context, input_list, process_one)				
-        self.assertEqual(result,['E','F'])
-
-    # 3- test that the function skips empty batches
-    def test_Skip_empty_batch(self):
-        print ("test Skip empty batch")
-        input_list = ['a','b','','','e','f','g','h']									
+    def test_batches(self):
+        log = []
+        def end_batch():				# appends x at the end of each batch
+            log.append('X')
+        from .. import batcher
         context = make_test_context(2,0,2)
-        from .. import batcher									
-        result = batcher.run_in_batches(context, input_list, process_one)				
-        self.assertEqual(result,['A','B'])
+        batcher = batcher.run_in_batches(		# reads from iterable and executes end_batch
+                context,
+                iter('abcdef'), end_batch)
+        for letter in batcher:				# takes the letter yielded by run_in_batches
+            log.append(letter)
+        self.assertEqual(
+                ''.join(log),
+                'abXcd')
 
-    # 4- test with arbitrary big max_batches number (will be set to 100 by default)
-    def test_Max_batches(self):
-        print ("test max batches")
-        input_list = ['a','b','','','e','f','g','h','i']									
-        context = make_test_context(2,0,100)
-        from .. import batcher									
-        result = batcher.run_in_batches(context, input_list, process_one)				
-        self.assertEqual(result,['A','B','E','F','G','H','I'])
-
-    # 5- test with arbitrary big max_batches number (will be set to 100 by default)
-    def test_Batch_size(self):
-        print ("test batch size")
-        input_list = ['a','b','c','d','e','f','g']									
-        context = make_test_context(3,0,3)
-        from .. import batcher									
-        result = batcher.run_in_batches(context, input_list, process_one)				
-        self.assertEqual(result,['A','B','C','D','E','F','G'])
-
-     # 6- test with csv file
-    def test_Batcher_input(self):
-        print ("test batcher input csv file")
-        filename = '/home/kiara/GIT/mp.importer/mp/importer/tests/test.csv'
-        openedfile = open(filename, 'r')
-        test_row_list = list()
-        for row in openedfile:
-            test_row = row[:-1]									# read the lines of the file and create a list, that will be then splitted 
-            test_row_list.append(test_row)
-        input_to_test = [test_row_list]   # try list(), set(), tuples
-        from .. import batcher	
-        context = make_test_context(2,0,3)
-        result_list = list()								
-        for i in input_to_test:
-            if hasattr(i, '__getitem__') is True: 						# __getitem__ is used by the batcher function! replaced by next!
-                result = batcher.run_in_batches(context, i, process_one)			
-                result_list.append(result) 
-            else:	
-                #print i									
-                pass
-                #self.fail()									# non-iterable fall in here -  test_set, test_frozenset, set()
-        self.assertEqual(result_list,[['CHIARA,ANNA', 'ANNA,CHIARA', 'XXX,', 'X']])	 # nb: takes the entire line of the csv
 
 
 
