0e5ec49fa44442a4414524fb6c30511113f3e9dd
kiara <saetachiara@gmail.com>
2015-03-27 12:56:31 +0100

    two TODOs with tests: start batching from a given one and make final batch with rest of elements

--- mp/importer/batcher.py
+++ mp/importer/batcher.py
@@ -7,32 +7,38 @@ from itertools import count
 
 def get_batches(context, iterable, nb):
     bsize = context.batch_size
-    ssize = context.start_batch
-    start = ssize*bsize
-    count = start+bsize*nb						# every time the counter must be set in order to retrieve the correct batch and for the generator to be looped consecutively
+    startbatch = context.start_batch
+    start = startbatch*bsize
+    count = start+bsize*nb			# after every break (end of batch) the counter must be re-set to retrieve the next one consecutively
+    print ('batch, count', nb, count)				
     for i in iterable:
-        count += 1
-        if start+bsize*nb <= count-1 <= start+bsize*nb+bsize:      	# filter for yielding - takes the nb-batch 
+        if start+bsize*nb <= count-start <= start+bsize*nb+bsize:      	# filter for yielding - takes the nb-batch 
             if count == start+bsize*nb+bsize:
                 print ("LAST batch element", i)
                 yield i
             else:
                 print ("batch element", i)
-                yield i            
+                yield i 
+        else:
+            print ("not in batch", i)
+            count += 1 
+            continue           
 
 def run_in_batches(context, iterable, end_batch):
     bmax = context.max_batches
     for nbatch in range(0,bmax):
         c = 0 
-        print ('nbatch', nbatch)
+        #print ('nbatch', nbatch)
         got_batch = get_batches(context, iterable, nbatch)		# gets batch elements (yields) for each value of nbatch - changes at every iteration
         for k in got_batch:
             c = c+1							# counter 					
-            print (c, k)
+            #print (c, k)
             yield k							# in the yielded batch, appends X in the right place
             if c < context.batch_size:
+                print "continue retrieving yielded batch elements"
                 continue						
-            else:
+            elif c == context.batch_size:
+                print "end batch"
                 end_batch()						# appends 'X' when batch ends
                 break
 
--- mp/importer/tests/test_batcher.py
+++ mp/importer/tests/test_batcher.py
@@ -17,6 +17,7 @@ def make_test_context(b_size, b_start, b_max):									# creates a "provisional"
 
 class TestBatcher(TestCase):
     def test_batches(self):
+        print ("*** test make batches")
         log = []
         def end_batch():				
             log.append('X')
@@ -30,6 +31,48 @@ class TestBatcher(TestCase):
         self.assertEqual(
                 ''.join(log),
                 'abXcdX')
+
+# test how to start batch from a given batch number - TODO
+    def test_Start_batches(self):
+        print ("*** test Start batches")
+        log = []
+        def end_batch():				
+            log.append('X')
+        from .. import batcher
+        context = make_test_context(2,1,2)
+        batcher = batcher.run_in_batches(		# reads from iterable., creates batches and executes end_batch at the end of each one
+                context,
+                iter('abcdefghilm'), end_batch)			
+        for letter in batcher:				# takes the letter yielded by run_in_batches
+            log.append(letter)
+        self.assertEqual(
+                ''.join(log),
+                'cdXefX')
+
+
+# test a batch that is incomplete . call end_batch when there are no elements left - TODO
+    def test_Rest_batches(self):
+        print ("*** test Rest batches")
+        log = []
+        def end_batch():				
+            log.append('X')
+        from .. import batcher
+        context = make_test_context(2,0,2)
+        batcher = batcher.run_in_batches(		# reads from iterable., creates batches and executes end_batch at the end of each one
+                context,
+                iter('abcde'), end_batch)			# ghilm
+        for letter in batcher:				# takes the letter yielded by run_in_batches
+            log.append(letter)
+        self.assertEqual(
+                ''.join(log),
+                'abXcdXeX')
+
+
+
+
+
+
+
 
 
 
