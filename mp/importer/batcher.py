0ca7cb76a21520f0eab68b0f40963552ae581909
kiara <saetachiara@gmail.com>
2015-03-30 11:54:40 +0200

    modified test functions - TODO add test for start_batch and modify the batcher according to Brian tips

--- mp/importer/batcher.py
+++ mp/importer/batcher.py
@@ -9,36 +9,38 @@ def get_batches(context, iterable, nb):
     bsize = context.batch_size
     startbatch = context.start_batch
     start = startbatch*bsize
-    count = bsize*nb
-    if nb == 0:		
+    count = 0
+    if nb == 0:								# the first batch		
         for i in iterable:
             count +=1
-            if start+bsize*nb <= count-1 <= start+bsize*nb+bsize:      	# filter for yielding - in this loop i skip the first batch of size "start" = startbatch*bsize
+            if start+bsize*nb <= count-1 <= start+bsize*nb+bsize:      	# skip the first batch of size "start" = startbatch*bsize
                 yield i
             else:
                 continue           
-    else:
+    else:								# build batches normally					
         for i in iterable:
             count +=1
             if bsize*nb <= count-1 <= bsize*nb+bsize: 
-                yield i     	 
+                yield i
             else:
                 continue
 
 def run_in_batches(context, iterable, end_batch):
     bmax = context.max_batches
-    for nbatch in range(0,bmax):
+    for nbatch in range(0,bmax):					# change according to Brian suggestion + max_batches default is None - TODO
         c = 0 
         got_batch = get_batches(context, iterable, nbatch)		# gets batch elements (yields) for each value of nbatch - changes at every iteration
         for k in got_batch:
             c = c+1							 					
-            yield k							
+            yield k
             if c < context.batch_size: 
                 continue						
             elif c == context.batch_size:
                 end_batch()						
                 break                     
-            # add if-else loop to end_batch() if batch is incomplete (it would be the last batch), now is in the test funcion
        
 
-     
+#current_batch, position_in_batch = divmod(count_of_total_items_iterated_over, batch_size)
+#current_batch += 1 # first batch is 1, but dived gives us zero
+#if position_in_batch == 0:
+    #end_batch()
--- mp/importer/tests/test_batcher.py
+++ mp/importer/tests/test_batcher.py
@@ -5,50 +5,44 @@ from collections import namedtuple
 from collections import Iterable
 from itertools import *
 
-def make_test_context(arg_list):									# creates a "provisional" context 
+def make_test_context(b_size, b_start, b_max):									# creates a "provisional" context 
     context = namedtuple('Context', """
         batch_size
         start_batch
         max_batches """)
-    context.batch_size = arg_list[0]
-    context.start_batch = arg_list[1]
-    context.max_batches = arg_list[2]
+    context.batch_size = b_size
+    context.start_batch = b_start
+    context.max_batches = b_max
     return context
 
-# dictionary of tests contains (test_name:result)
-test_dict = {'Make batches':'abXcdX','Max Batches':'abXcdXefXghXilXmX','Batch size':'abcXdefX','Start batch':'cdXefXghX','Last incomplete batch':'abXcdXeX'}
-
-# dictionary of context parameters
-context_dict = {'Make batches':(2,0,2),'Max Batches':(2,0,10),'Batch size':(3,0,2),'Start batch':(2,1,3),'Last incomplete batch':(2,0,3)}
-
 class TestBatcher(TestCase):
-    def test_for_batches(self):
+
+    def one(self, source, batch_size=2, batch_start=0, max_batches=None):
         log = []
-        def end_batch():				
+        def end_batch():                
             log.append('X')
         from .. import batcher
-        input_string = 'abcdefghilm'			#input is always the same for these tests 
-        for test,result in test_dict.items():
-            args = context_dict[test]
-            print ("args list", args)
-            context = make_test_context(args)
-            print ("executing test:", test) 
-            batcher = batcher.run_in_batches(		
-                    context,
-                    iter(input_string), end_batch)			
-            for letter in batcher:				
-                log.append(letter)
-            if log[-1] != 'X':
-                end_batch()
-            self.assertEqual(
-                    ''.join(log),
-                    result) 
-            break 					# if only once, it works, but cannot be looped because the generator is exahusted. need to restart it somehow TODO
+        context = make_test_context(batch_size, batch_start, max_batches)
+        batcher = batcher.run_in_batches(       
+                context,
+                source, end_batch)          
+        for letter in batcher:              
+            log.append(letter)
+        if log[-1] != 'X':
+            end_batch()
+        return ''.join(log)
 
+    def test_batches(self):
+        result = self.one('abcdefg', max_batches=100)			# check it with None when changed the code 
+        self.assertEqual(result, 'abXcdXefXgX')
 
+    def test_Max_batches(self):
+        result = self.one('abcdefg', max_batches=2)
+        self.assertEqual(result, 'abXcdX')
 
-
-
+    #def test_Start_batch(self):
+    #    result = self.one('abcdefg', batch_start=1, max_batches=2)
+    #    self.assertEqual(result, 'cdXefX')
 
 
 
