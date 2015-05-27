75c813d7584fab7ae10ad3140ba6ebeb38dbec04
kiara <saetachiara@gmail.com>
2015-05-27 14:25:36 +0200

    now it works also for max_batches=None although the function is longer

--- mp/importer/batcher.py
+++ mp/importer/batcher.py
@@ -1,22 +1,37 @@
 
-def run_in_batches(iterable, end_batch_callback=None, batch_size=2, batch_start=0, max_batches=10000):		# what if max_batches = None? TODO
+def run_in_batches(iterable, end_batch_callback=None, batch_size=2, batch_start=0, max_batches=None):		# what if max_batches = None? TODO
     c = 0
     ended = False
-    for k in iterable:
-        c = c + 1			
-        current_batch, position_in_batch = divmod(c, batch_size)
-        if position_in_batch == 0:
-            current_batch = current_batch - 1						# last element of a batch has position=0, value=current_batch+1
-        if current_batch >= batch_start and current_batch < batch_start+max_batches:
-            ended = False
-            yield k 
-            if position_in_batch == 0: 							# ends completed batches
-                end_batch_callback()
-                ended = True
-        if current_batch == batch_start+max_batches:					# if gets maximum allowed batches, break 
-            break
-    if ended is False:
-        end_batch_callback()	
+    if max_batches is None:
+        for k in iterable:
+            c = c + 1			
+            current_batch, position_in_batch = divmod(c, batch_size)
+            if position_in_batch == 0:
+                current_batch = current_batch - 1
+            if current_batch >= batch_start:
+                ended = False
+                yield k 
+                if position_in_batch == 0: 							# ends completed batches
+                    end_batch_callback()
+                    ended = True
+        if ended is False:
+            end_batch_callback()
+    else:
+        for k in iterable:
+            c = c + 1			
+            current_batch, position_in_batch = divmod(c, batch_size)
+            if position_in_batch == 0:
+                current_batch = current_batch - 1						# last element of a batch has position=0, value=current_batch+1
+            if current_batch >= batch_start and current_batch < batch_start+max_batches:
+                ended = False
+                yield k 
+                if position_in_batch == 0: 							# ends completed batches
+                    end_batch_callback()
+                    ended = True
+            if current_batch == batch_start+max_batches:					# if gets maximum allowed batches, break 
+                break
+        if ended is False:
+            end_batch_callback()		
 
 
 def run_in_batches_with_options(iterable, end_batch_callback, options): 		# arguments can be passed through "options"
