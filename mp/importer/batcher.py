931739427bb1adf7a8bb7eaae4d5db6f485aabbd
kiara <saetachiara@gmail.com>
2015-05-27 12:59:17 +0200

    use *kw to pass keyworkds

--- mp/importer/batcher.py
+++ mp/importer/batcher.py
@@ -1,25 +1,25 @@
 
-def run_in_batches(iterable, end_batch_callback, batch_size, start_batch, max_batches):					# arguments passed through "options"
+def run_in_batches(iterable, end_batch_callback=None, batch_size=1000, batch_start=0, max_batches=None):		# arguments passed through "options", set default - NB batch_size!=0
     c = 0
     ended = False
     for k in iterable:
-        c = c+1			
+        c = c + 1			
         current_batch, position_in_batch = divmod(c, batch_size)
         if position_in_batch == 0:
-            current_batch = current_batch-1						# last element of a batch has position=0, value=current_batch+1
-        if current_batch >= start_batch and current_batch < start_batch+max_batches:
+            current_batch = current_batch - 1						# last element of a batch has position=0, value=current_batch+1
+        if current_batch >= batch_start and current_batch < batch_start+max_batches:
             ended = False
             yield k 
             if position_in_batch == 0: 							# ends completed batches
                 end_batch_callback()
                 ended = True
-        if current_batch == start_batch+max_batches:						# if gets maximum allowed batches, break 
+        if current_batch == batch_start+max_batches:						# if gets maximum allowed batches, break 
             break
     if ended is False:
         end_batch_callback()	
 
 
-def run_in_batches_with_options(iterable, end_batch_callback, options):
-    for x in run_in_batches (iterable, end_batch_callback, options.batch_size, options.start_batch, options.max_batches):
+def run_in_batches_with_options(iterable, end_batch_callback, options): 	
+    for x in run_in_batches(iterable, end_batch_callback, options.batch_size, options.batch_start, options.max_batches):	# use **kw here as well? so not specified options are default
         yield x
 
--- mp/importer/tests/test_batcher.py
+++ mp/importer/tests/test_batcher.py
@@ -5,51 +5,43 @@ from collections import namedtuple
 from collections import Iterable
 from itertools import *
 
-def make_test_options(b_size, b_start, b_max):									# creates a "provisional" options
-    options = namedtuple('Options', """
-        batch_size
-        start_batch
-        max_batches """)
-    options.batch_size = b_size
-    options.start_batch = b_start
-    options.max_batches = b_max
+def make_test_options(batch_size=2, batch_start=0, max_batches=10):							# creates a "provisional" options, set default in case **kw none 
+    options = namedtuple('Options', "batch_size, start_batch, max_batches")
+    options.batch_size = batch_size
+    options.batch_start = batch_start
+    options.max_batches = max_batches
     return options
 
 class TestBatcher(TestCase):
 
-    def one(self, source, batch_size=2, batch_start=0, max_batches=10):					# batch size cannot be 0
+    def one(self, source, **kw): 								# **kw allows passing only some keyworks (the others will be set to default by batcher) -easier for user
         log = []
         def end_batch():                
             log.append('X')
         from .. import batcher
-        options = make_test_options(batch_size, batch_start, max_batches)
-        batcher = batcher.run_in_batches_with_options(source, end_batch, options)
+        options = make_test_options(**kw)							#batch_size, batch_start, max_batches)
+        batcher = batcher.run_in_batches_with_options(source, end_batch, options) 		
         for letter in batcher:              
             log.append(letter)
         return ''.join(log)
 
     def test_Make_batches(self):
-        print ("*** launch Make_batches")
-        result = self.one('abcdefg')			# check it with None when changed the code (default is None)
+        result = self.one('abcdefg')			
         self.assertEqual(result, 'abXcdXefXgX')		
 
     def test_Max_batches(self):
-        print ("*** launch Max_batches")
         result = self.one('abcdefg', max_batches=2)
         self.assertEqual(result, 'abXcdX')
 
     def test_Batch_size(self):
-        print ("*** launch Batch_size")
         result = self.one('abcdefg', batch_size=3, max_batches=2)
         self.assertEqual(result, 'abcXdefX')
 
     def test_Start_batch(self):
-        print ("*** launch Start_batch")
-        result = self.one('abcdefg', batch_start=2, max_batches=3)  # batch_start=1 means skipping the first batch (the 0-batch)
+        result = self.one('abcdefg', batch_start=2, max_batches=3)  				# batch_start=1 means skipping the first batch (the 0-batch)
         self.assertEqual(result, 'efXgX')		
 
     def test_End_iterator(self):
-        print ("*** launch End_iterator")
         result = self.one('abc', batch_size=2, max_batches=2)
         self.assertEqual(result, 'abXcX')
 
